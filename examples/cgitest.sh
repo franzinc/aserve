f96812cb78c894a2dac2ff924012ed40b7fe12c9
branch-fixup <branch-fixup>
2001-10-17 22:32:44 +0000

    Add files from parent branch HEAD: 	cgi.cl 	examples/cgitest.sh

---
+++ examples/cgitest.sh
@@ -0,0 +1,44 @@
+#! /bin/sh
+#
+# return various cgi responses based on the first argument
+#
+case $1 in
+
+    1) # bogus but common lf headers
+        echo 'Content-Type: text/plain'
+        echo 
+        echo "The environment vars are "
+        env
+        echo "==== end ===="
+        ;;
+
+    2) # redirect to franz.com, send some headers
+        echo 'Location: http://www.franz.com'
+        echo 'etag: 123hellomac'
+	echo
+	echo -n 'go to franz'
+        ;;
+
+    3) # send back unauthorized request
+        echo 'Status: 401 unauthorized request'
+	echo
+        echo 'this request unauthorized'
+        ;;
+
+    4) # send back an ok response and something on the error stream
+        echo 'Content-Type: text/plain'
+        echo ''
+        echo "okay"
+        echo stuff-on-error-stream 1>&2
+	;;
+        
+    *) # normal crlf headers
+        echo 'Content-Type: text/plain'
+        echo ''
+        echo "The environment vars are "
+        env
+        echo "==== end ===="
+        ;;
+esac
+    
+
