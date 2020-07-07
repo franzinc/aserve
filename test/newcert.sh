b5fd530fee23128f9c792bcb428c1c245d17620c
Kevin Layer <layer@franz.com>
2020-07-07 07:42:53 -0700

    test/newcert.sh: no longer asks for user input
    No reason for it to ask for user input, so make it so.
    
    %Testing: manual
    
    Change-Id: Idfc468484784a87c8e49dd72d5dea2baba038980
    Reviewed-on: https://gerrit.franz.com:9080/c/aserve/+/16805
    Tested-by: Kevin Layer <layer@franz.com>
    Reviewed-by: John Foderaro <jkf@franz.com>
    Reviewed-by: Kevin Layer <layer@franz.com>

--- test/newcert.sh
+++ test/newcert.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 openssl req -newkey rsa:4096 -new -nodes -x509 -days 3650 \
+	-subj "/C=US/ST=US/L=Lafayette/O=Franz Inc/OU=Com/CN=www.franz.com" \
 	-keyout private.pem -out temp.pem
 
 cat private.pem temp.pem > server.pem
